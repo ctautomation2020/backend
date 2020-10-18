952546ea0586fcaf6512e9ab2569a9d99b5fe696
keerthi <kpkeerthi34@gmail.com>
2020-10-18 18:20:08 +0530

    update issue

--- src/prisma.js
+++ src/prisma.js
@@ -3,19 +3,20 @@ const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 
 const fun = async()=> {
-    console.log(await prisma.person_qualification.create({
-        data:{
-            person:{
-                connect:{
+    console.log(await prisma.person.update({
+        where:{
+            
                     Person_ID:1
-                }
-            },
-            person_reference_table_person_qualification_Branch_RefToperson_reference_table:{
-                connect:{
-                    Reference_ID:null
+                },
+            data:{
+                person_reference_table_person_Community_RefToperson_reference_table:{
+                    connect:{
+                        Reference_ID:4
+                    }
                 }
             }
-        }
+            
+        
     }))
 }
 
