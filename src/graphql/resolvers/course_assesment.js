b442a1af700f2eb8eb572452c3158e327d911e42
keerthi <kpkeerthi34@gmail.com>
2020-12-18 18:25:25 +0530

    assess query

--- src/graphql/resolvers/course_assesment.js
+++ src/graphql/resolvers/course_assesment.js
@@ -1,9 +1,9 @@
 module.exports = {
     Query: {
         async assesment(parent, {data}, {prisma}, info){
-            asses = await prisma.course_assessment.findMany({
+            return await prisma.course_assessment.findMany({
                 where:{
-                    cassess_id: data.cassess_id
+                    ...data
                 }
             })
             
