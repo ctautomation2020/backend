23039fc3709e5abbac153c2d080834c5854a83a4
keerthi <kpkeerthi34@gmail.com>
2021-02-15 18:22:29 +0530

    internal_calc data population

--- src/graphql/resolvers/course_cacomp.js
+++ src/graphql/resolvers/course_cacomp.js
@@ -31,9 +31,14 @@ module.exports = {
             //     throw new Error("ca_comp already created")
             // }
 
+            var midterm_number =1
+
             remData.compques.forEach(async (ques) => {
                     let tynum = ques.type.toString()+ques.number.toString()
+                    
                     myMap.set(tynum,ques.weightage)
+                    
+                    
                     await prisma.course_cacomp.create({
                         data: {
                             course_list: {
@@ -54,7 +59,8 @@ module.exports = {
                             ...ques
                         }
                     })
-            
+                    
+                    
             })
 
             const stud_marks = await prisma.course_evaluation.findMany({
@@ -81,7 +87,9 @@ module.exports = {
                 
                 temp.forEach(async(marks)=>{
                     
-                    if(myMap.get(marks.type.toString()+marks.number.toString())){
+                    const map_idx = myMap.get(marks.type.toString()+marks.number.toString())
+
+                    if(map_idx ){
 
                         let weighted_mark = marks.marks_obtained / marks.total_mark * myMap.get(marks.type.toString()+marks.number.toString())
                         
@@ -103,28 +111,94 @@ module.exports = {
                         })
                     }
 
-                    else if(marks.type==3) {
+                    else if(marks.type==1 && myMap.get("3"+marks.number.toString())) {
+                        //console.log("hr",stud.reg_no)
                         await prisma.course_evaluation.updateMany({
                             where:{
                                 course_code, 
                                 group_ref,
                                 session_ref,
-                                reg_no: marks.reg_no,
-                                type: marks.type,
+                                reg_no: stud.reg_no,
+                                type: {in:[1,3]},
                                 number: marks.number,
-
+    
                             },
-
+    
                             data: {
+                                type: 3,
                                 weighted_mark: marks.marks_obtained / marks.total_mark * 100
                             }
-
+    
                         })
                     }
 
                 })
+
                 
+
+                const ca = await prisma.course_evaluation.aggregate({
+                    sum:{
+                        weighted_mark:true
+                    },
+                    where:{
+                        course_code, 
+                        group_ref,
+                        session_ref,
+                        reg_no: stud.reg_no,
+                        type:{not:3}
+                    }
+                })
+                
+                const midterm = await prisma.course_evaluation.aggregate({
+                    sum:{
+                        weighted_mark:true
+                    },
+                    where:{
+                        course_code, 
+                        group_ref,
+                        session_ref,
+                        reg_no: stud.reg_no,
+                        type:3
+                    }
+                })
+
+                //const midterm = 100
+
+                const total_marks = ca.sum.weighted_mark/100*60 + midterm.sum.weighted_mark/100 *40
+
+                await prisma.course_internalcalc.create({
+
+                    data:{
+                        course_list:{
+                            connect: {
+                                course_code
+                            }
+                        },
+                        course_reference_table_course_internalcalc_group_refTocourse_reference_table:{
+                            connect: {
+                                ref_code: group_ref
+                            }
+                        },
+                        course_reference_table_course_internalcalc_session_refTocourse_reference_table:{
+                            connect: {
+                                ref_code: session_ref
+                            }
+                        },
+                        student:{
+                            connect: {
+                                Register_No:stud.reg_no
+                            }
+                        },
+                        ca:ca.sum.weighted_mark,
+                        midterm:midterm.sum.weighted_mark,
+                        total_marks:total_marks
+                    }
+                   
+                })
+
             })
+
+            
 
             return flag;
 
